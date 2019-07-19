25ae69806612bfa1f213bc140fb4d493321963f3
Rob Tuinte <robtuinte15@gmail.com>
2019-07-19 13:14:00 +0200

    finished

--- src/index.jsx
+++ src/index.jsx
@@ -3,20 +3,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
+import reducers from './reducers';
 
 // internal modules
 import App from './components/app';
 import '../assets/stylesheets/application.scss';
 
 // State and reducers
-const reducers = combineReducers({
-  changeMe: (state = null, action) => state
-});
+// const reducers = combineReducers({
+//   // cities: citiesReducer
+// });
 
 // render an instance of the component in the DOM
-ReactDOM.render(
-  <Provider store={createStore(reducers)}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <Provider store={createStore(reducers)}>
+//     <App />
+//   </Provider>,
+//   document.querySelector('.container')
+// );
+
+
+const root = document.getElementById('root');
+if (root) {
+  ReactDOM.render(
+    <Provider store={createStore(reducers)}>
+      <App />
+    </Provider>,
+    root
+  );
+}
